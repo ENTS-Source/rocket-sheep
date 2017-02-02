1bcac05d444501bee0ff9bf7e08b717e4833e482
turt2live <travpc@gmail.com>
2017-02-01 19:18:01 -0700

    Use pip for own dependencies instead of a patch file

--- debug-run.sh
+++ debug-run.sh
@@ -6,5 +6,6 @@ rm -rf .bot
 mv .tmp .bot
 cd .bot
 ../env/Scripts/python setup.py install
+../env/Scripts/pip install -r ../requirements.txt
 ../env/Scripts/python neb.py -c neb.json
 read -rsp $'Press enter to continue...\n'
